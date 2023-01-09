bf2e501a2b3f59e885156767d8c7f5df4d3587e5
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2023-01-09 22:43:34 +0100

    fix: stadistic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,22 @@ import '../styles/globals.css';
 import 'highlight.js/styles/github.css';
 import type { AppProps } from 'next/app';
 
+import splitbee from '@splitbee/web';
+
+// This initiliazes Splitbee.js
+splitbee.init({
+	// To use Splitbee on another subdomain.
+	// Token can be found in project settings
+	token: 'CJ4FN2JHQFQO',
+
+	// Enable cookie-less mode. Defaults to `false`
+	disableCookie: false,
+
+	// Set custom urls when using proxying
+	scriptUrl: 'https://cdn.splitbee.io/sb.js',
+	apiUrl: 'https://hive.splitbee.io',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
