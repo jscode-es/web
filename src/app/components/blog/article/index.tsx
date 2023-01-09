d50d1ea0594179482e72dcd608b8e67bb954ff8e
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2023-01-09 20:46:47 +0100

    fix: to metadata on post

--- src/app/components/blog/article/index.tsx
+++ src/app/components/blog/article/index.tsx
@@ -2,6 +2,7 @@ import style from './style.module.css';
 import { Publish } from '../publish';
 import Image from 'next/image';
 import { useState } from 'react';
+import Head from 'next/head';
 
 export function Article({
 	data: { title, subtitle, description, content, slug },
@@ -9,27 +10,55 @@ export function Article({
 	const [src, setSrc] = useState(`/portrait/${slug}.png`);
 
 	return (
-		<div className={style.container}>
-			<div className={style.headline}>
-				<h3>{subtitle}</h3>
-				<h1 dangerouslySetInnerHTML={{ __html: title }} />
-				<div className={style.portrait}>
-					<Image
-						src={src}
-						alt={description}
-						layout='fill'
-						blurDataURL='/assets/empty.png'
-						placeholder='blur'
-						onError={() => setSrc('/assets/empty.png')}
-					/>
+		<>
+			<Head>
+				<meta
+					property='og:type'
+					content='article'
+				/>
+				<meta
+					property='og:title'
+					content={title.replace('<br>', ' ')}
+				/>
+				<meta
+					property='og:description'
+					content={description}
+				/>
+				<meta
+					property='og:image'
+					content={`https://jscode.es/${src}`}
+				/>
+				<meta
+					property='og:url'
+					content={`https://jscode.es/${slug}`}
+				/>
+				<meta
+					property='og:site_name'
+					content='JSCode'
+				/>
+			</Head>
+			<div className={style.container}>
+				<div className={style.headline}>
+					<h3>{subtitle}</h3>
+					<h1 dangerouslySetInnerHTML={{ __html: title }} />
+					<div className={style.portrait}>
+						<Image
+							src={src}
+							alt={description}
+							layout='fill'
+							blurDataURL='/assets/empty.png'
+							placeholder='blur'
+							onError={() => setSrc('/assets/empty.png')}
+						/>
+					</div>
 				</div>
+				<Publish />
+				<div
+					className={style.content}
+					dangerouslySetInnerHTML={{ __html: content }}
+				/>
+				<Publish />
 			</div>
-			<Publish />
-			<div
-				className={style.content}
-				dangerouslySetInnerHTML={{ __html: content }}
-			/>
-			<Publish />
-		</div>
+		</>
 	);
 }
