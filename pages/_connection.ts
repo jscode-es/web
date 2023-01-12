8a93c6c3c580d49dc87b3728af3e038a754acd01
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2023-01-13 00:01:29 +0100

    Update _connection.ts

--- pages/_connection.ts
+++ pages/_connection.ts
@@ -1,4 +1,4 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+/* import { MongoClient, ServerApiVersion } from 'mongodb'; */
 
 export class Database {
 	private readonly uri =
@@ -8,21 +8,20 @@ export class Database {
 	private table: string = '';
 
 	constructor({ collection, table }: any) {
-		this.client = new MongoClient(this.uri, {
+		/* 	this.client = new MongoClient(this.uri, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			serverApi: ServerApiVersion.v1,
 		} as any);
 
 		this.collection = collection;
-		this.table = table;
+		this.table = table; */
 	}
 
 	async connect() {
-		this.client.connect(async (err: any) => {
+		/* this.client.connect(async (err: any) => {
 			Promise.resolve(true);
-		});
-
+		}); */
 		/*  try {
            
             const db: Db = client.db('<dbname>');
@@ -38,11 +37,12 @@ export class Database {
 
 	async find(params: Record<string, unknown> = {}) {
 		const db = this.client.db(this.table);
-		const table = db.collection(this.collection);
+		/* const table = db.collection(this.collection);
 		const result = await table.find(params).toArray();
 
 		this.client.close();
 
-		return result;
+		return result; */
+		return [];
 	}
 }
