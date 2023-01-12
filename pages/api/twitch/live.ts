476687a44a2853c51d9d713a7338952cebb2efc5
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2023-01-13 00:11:04 +0100

    change

--- pages/api/twitch/live.ts
+++ pages/api/twitch/live.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Database } from '../../_connection';
+import { Database } from '../../../utils/connection';
 
 type Data = Record<string, unknown>;
 
