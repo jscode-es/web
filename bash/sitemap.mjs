3f15f137e65732f3ff7c8e844cae1dd022927b62
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2023-01-20 13:53:09 +0100

    update

--- bash/sitemap.mjs
+++ bash/sitemap.mjs
@@ -34,7 +34,7 @@ const generate = async () => {
         </url>`;
 	}
 
-	const content = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">${urls}</urlset>`;
+	const content = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">${urls}</urlset>`;
 
 	fs.writeFile(publicPath, content, { encoding: 'utf8', flag: 'w' });
 };
