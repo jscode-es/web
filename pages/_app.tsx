9aac47053778e9d3dcc4a21ca34da47b1fe8980d
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2023-01-21 01:50:02 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,28 +3,16 @@ import type { AppProps } from 'next/app';
 import '../styles/fonts.css';
 import '../styles/globals.css';
 
-import splitbee from '@splitbee/web';
 import { useEffect } from 'react';
+import { Statistics } from '../src/infrastruture/repository/statistics';
+import { SocketProvider } from '../src/app/context/socket';
 
-// This initiliazes Splitbee.js
-splitbee.init({
-	// To use Splitbee on another subdomain.
-	// Token can be found in project settings
-	token: 'VL0GI9KKLT45',
-
-	// Enable cookie-less mode. Defaults to `false`
-	disableCookie: false,
-
-	// Set custom urls when using proxying
-	scriptUrl: 'https://cdn.splitbee.io/sb.js',
-	apiUrl: 'https://hive.splitbee.io',
-});
+Statistics.init();
 
 export default function App({ Component, pageProps }: AppProps) {
-	useEffect(() => {
-		(global as any).HOST = location.origin;
-		(global as any).HOST_SOCKET = process.env.NEXT_PUBLIC_SOCKET;
-	}, []);
-
-	return <Component {...pageProps} />;
+	return (
+		<SocketProvider>
+			<Component {...pageProps} />
+		</SocketProvider>
+	);
 }
