a1082312e81cde7c47afe92ddc839b3bfe61d608
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2023-07-28 20:45:41 +0200

    balance text

---
+++ src/app/components/heading/index.tsx
@@ -0,0 +1,81 @@
+export function H1({ children }: { children: any}) { 
+    return (
+        <h1 className=''>
+            {children}
+        </h1>
+    );
+}
+
+export function H2({ children }: { children: any }) {
+    return (
+        <h2 className=''>
+            {children}
+        </h2>
+    );
+}
+
+export function H3({ children }: { children: any }) {
+    return (
+        <h3 className=''>
+            {children}
+        </h3>
+    );
+}
+
+export function H4({ children }: { children: any }) {
+    return (
+        <h4 className=''>
+            {children}
+        </h4>
+    );
+}
+
+export function H5({ children }: { children: any }) {
+    return (
+        <h5 className=''>
+            {children}
+        </h5>
+    );
+}
+
+export function H6({ children }: { children: any }) {
+    return (
+        <h6 className=''>
+            {children}
+        </h6>
+    );
+}
+
+export function P({ children }: { children: any }) {
+    return (
+        <p className=''>
+            {children}
+        </p>
+    );
+}
+
+export function Span({ children }: { children: any }) {
+    return (
+        <span className=''>
+            {children}
+        </span>
+    );
+}
+
+export function Strong({ children }: { children: any }) {
+    return (
+        <strong className=''>
+            {children}
+        </strong>
+    );
+}
+
+export function Small({ children }: { children: any }) {
+    return (
+        <small className=''>
+            {children}
+        </small>
+    );
+}
+
+
